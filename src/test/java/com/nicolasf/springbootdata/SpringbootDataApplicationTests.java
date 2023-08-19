fcf4fe7333acad845e6127ba484ee21ef18d7053
Nicolas <nicolas@nicolasf.uy>
2023-08-18 23:52:30 -0300

    Start section 6

---
+++ src/test/java/com/nicolasf/springbootdata/SpringbootDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasf.springbootdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
